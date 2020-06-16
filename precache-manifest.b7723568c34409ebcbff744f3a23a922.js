2869d74dadc4fca448fc6bccd38c77c99faf9a9d
neilshah20 <neilshah20@gmail.com>
2020-06-16 18:05:35 -0400

    Updates

---
+++ precache-manifest.b7723568c34409ebcbff744f3a23a922.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fcb8434b8491e43f1c1ce3c33b3242f",
+    "url": "/gimmedafacts/index.html"
+  },
+  {
+    "revision": "f89d08a7cf59ededb9fc",
+    "url": "/gimmedafacts/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "82b1df5a0e1f62a97eff",
+    "url": "/gimmedafacts/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f89d08a7cf59ededb9fc",
+    "url": "/gimmedafacts/static/js/2.5584c936.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gimmedafacts/static/js/2.5584c936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82b1df5a0e1f62a97eff",
+    "url": "/gimmedafacts/static/js/main.529f5cf4.chunk.js"
+  },
+  {
+    "revision": "75e63ab871e72c2fa794",
+    "url": "/gimmedafacts/static/js/runtime-main.a09f9410.js"
+  }
+]);
